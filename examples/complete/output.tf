3b576fca636250bef3eadba739c171f6e525c6e8
RohitSquareops <rohit.kumar@squareops.com>
2023-05-10 10:53:13 +0530

    Updated variables,readme, indestation

--- examples/complete/output.tf
+++ examples/complete/output.tf
@@ -1,21 +1,24 @@
 output "rabbitmq_endpoint" {
   value       = module.rabbitmq.rabbitmq_endpoint
-  description = "rabbitmq pod connection endpoint"
+  description = "Endpoint of the RabbitMQ pod that can be used to connect to the message broker."
 }
 
 output "rabbitmq_headless_endpoint" {
-  value = module.rabbitmq.rabbitmq_headless_endpoint
+  value       = module.rabbitmq.rabbitmq_headless_endpoint
+  description = "Headless endpoint of the RabbitMQ pod that can be used to connect to the message broker."
 }
 
 output "rabbitmq_AMQP_port" {
   value       = "5672"
-  description = "rabbitmq AMQP(Advanced Message Queuing Protocol) port"
+  description = "Port number on which the RabbitMQ pod exposes the AMQP (Advanced Message Queuing Protocol) interface."
 }
 
 output "rabbitmq_management_interface_port" {
-  value = "15672"
+  value       = "15672"
+  description = "Port number on which the RabbitMQ pod exposes its web-based management interface, which can be used to monitor and manage the message broker."
 }
 
 output "rabbitmq_management_hostname" {
-  value = module.rabbitmq.rabbitmq_management_hostname
+  value       = module.rabbitmq.rabbitmq_management_hostname
+  description = "Hostname that can be used to access the RabbitMQ management interface from outside the Kubernetes cluster."
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 variable "rabbitmq_config" {
-  type = any
   default = {
     name               = ""
     environment        = ""
@@ -8,43 +7,43 @@ variable "rabbitmq_config" {
     volume_size        = ""
     hostname           = ""
     values_yaml        = ""
-
   }
-  description = "Rabbitmq configurations"
+  type        = any
+  description = "Specify the configuration settings for Redis, including the name, environment, storage options, replication settings, and custom YAML values."
 }
 
 variable "username" {
   default     = "admin"
   type        = string
-  description = "Enter user name"
+  description = "Username that will be used for authentication when connecting to the RabbitMQ cluster."
 }
 
 variable "namespace" {
   default     = "rabbitmq"
   type        = string
-  description = "Enter namespace name"
+  description = "Name of the Kubernetes namespace where the RabbitMQ deployment will be deployed."
 }
 
 variable "chart_version" {
   default     = "10.3.5"
   type        = string
-  description = "Chart version of rabbitmq"
+  description = "Version of the RabbitMQ chart that will be used to deploy the message broker."
 }
 
 variable "rabbitmq_exporter_enabled" {
   default     = true
   type        = bool
-  description = "Set true to deploy rabbitmq exporter to get metrics in grafana"
+  description = "Specify whether or not to deploy Redis exporter to collect Redis metrics for monitoring in Grafana."
 }
 
 variable "recovery_window_aws_secret" {
   default     = 0
   type        = number
-  description = "Number of days that AWS Secrets Manager waits before it can delete the secret. This value can be 0 to force deletion without recovery or range from 7 to 30 days."
+  description = "Number of days that AWS Secrets Manager will wait before deleting a secret. This value can be set to 0 to force immediate deletion, or to a value between 7 and 30 days to allow for recovery."
 }
 
 variable "create_namespace" {
-  type        = string
-  description = "Set it to true to create given namespace"
   default     = true
+  type        = string
+  description = "Specify whether or not to create the namespace if it does not already exist. Set it to true to create the namespace."
 }
