c3ac00e428f1470c101406b04e20f296eb98f2b2
prajwalakhuj <prajwalakhuj21@gmail.com>
2023-08-24 15:43:53 +0530

    Added support for Azure

---
+++ examples/complete/azure/main.tf
@@ -0,0 +1,46 @@
+locals {
+  name        = "rabbitmq"
+  region      = "eastus"
+  environment = "prod"
+  additional_tags = {
+    Owner      = "organization_name"
+    Expires    = "Never"
+    Department = "Engineering"
+  }
+  store_password_to_secret_manager = true
+  custom_credentials_enabled       = false
+  custom_credentials_config = {
+    rabbitmq_password     = "aa0z1IoRjOgRuon3aG",
+    erlangcookie_password = "bbddff0z1IoRuon3aG"
+  }
+}
+
+module "azure" {
+  source                           = "https://github.com/sq-ia/terraform-kubernetes-rabbitmq.git//provider/azure"
+  resource_group_name              = ""
+  resource_group_location          = local.region
+  environment                      = local.environment
+  name                             = local.name
+  username                         = "admin" # Rabbitmq username
+  store_password_to_secret_manager = local.store_password_to_secret_manager
+  custom_credentials_enabled       = local.custom_credentials_enabled
+  custom_credentials_config        = local.custom_credentials_config
+}
+
+module "rabbitmq" {
+  source = "https://github.com/sq-ia/terraform-kubernetes-rabbitmq.git"
+  rabbitmq_config = {
+    name                             = local.name
+    environment                      = local.environment
+    values_yaml                      = file("./helm/values.yaml")
+    volume_size                      = "50Gi"
+    replica_count                    = 2
+    storage_class_name               = "infra-service-sc"
+    store_password_to_secret_manager = local.store_password_to_secret_manager
+  }
+  rabbitmq_exporter_enabled  = true
+  custom_credentials_enabled = local.custom_credentials_enabled
+  custom_credentials_config  = local.custom_credentials_config
+  rabbitmq_password          = local.custom_credentials_enabled ? "" : module.azure.rabbitmq_password
+  erlangcookie_password      = local.custom_credentials_enabled ? "" : module.azure.erlangcookie_password
+}
